463bcac443b28e438e36f38ce9cdb8f1da29f437
salutlespotes <alexis.amiand@gmail.com>
2016-11-10 18:19:38 +0100

    Pour Caroline

--- src/JUtest/ItemTest.java
+++ src/JUtest/ItemTest.java
@@ -1,33 +1,39 @@
-package JUtest;
 import static org.junit.Assert.*;
-import wotr.*;
+package JUtest;
 import org.junit.Test;
+import wort.*;
 
 public class ItemTest {
 
+	private Item myItem;
+
+	protected void setUp() {
+		myItem = new Item ("Bier","I give health points.",10,player1);
+	}
+
 	@Test
 	public void testItem() {
-		fail("Not yet implemented");
+		assertNotNull("Instance is not created.", myItem);
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Not yet implemented");
+		assertEquals("Bier",myItem.getName());
 	}
 
 	@Test
 	public void testGetDescription() {
-		fail("Not yet implemented");
+		assertEquals("I give health points.",myItem.getDescription());
 	}
 
 	@Test
 	public void testGetWeight() {
-		fail("Not yet implemented");
+		assertEquals(10,myItem.getWeight());
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		assertEquals("Name : Bier\nDescription : I give health points.\nWeight : 10",myItem.toString());
 	}
 
 	@Test
