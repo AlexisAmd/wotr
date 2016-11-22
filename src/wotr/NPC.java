b0f1685bb1ebed2138e91993a0d20f35356fb50e
salutlespotes <alexis.amiand@gmail.com>
2016-11-22 09:31:54 +0100

    Class NPC : modifying some methods attributes

--- src/wotr/NPC.java
+++ src/wotr/NPC.java
@@ -70,7 +70,7 @@ public  class NPC
 /**
 *Method to set the alreadyused attributes to true or false. Mostly used by object from class "Magician"
 */
-    protected void setAlreadyUsed(boolean bool){
+    public void setAlreadyUsed(boolean bool){
         this.alreadyUsed=bool;
     }
 
@@ -100,7 +100,7 @@ public  class NPC
 /**
     *Restore the power of this NPC (used by the magician)
     */
-    private void restorePower(){
+    public void restorePower(){
         this.setAlreadyUsed(false);
     }
 
