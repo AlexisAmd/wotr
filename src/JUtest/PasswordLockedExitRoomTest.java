9aabddeaeeac1a936f8a9bb534dc58c02a3f3df7
adayre <ant.dayre@gmail.com>
2016-11-22 14:44:13 +0100

    WIP Antoine Password Locked

--- src/JUtest/PasswordLockedExitRoomTest.java
+++ src/JUtest/PasswordLockedExitRoomTest.java
@@ -1,10 +1,21 @@
+//WIP Antoine
 package JUtest;
+import wotr.*;
 import static org.junit.Assert.*;
 import wotr.*;
 import org.junit.Test;
 
 public class PasswordLockedExitRoomTest {
+	
+	private Room currentRoom, nextRoom;
+	private PasswordLockedExitRoom passwordLocked;
+	
 
+	@Before
+	public void setUp(){
+		currentRoom = new Room("salle1");
+		nextRoom = new Room("salle2");
+	}
 	@Test
 	public void testGetNextRoom() {
 		fail("Not yet implemented");
