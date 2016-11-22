5460c65cb6d7ddda5746a332ce873d21032c8a2c
salutlespotes <alexis.amiand@gmail.com>
2016-11-22 16:44:15 +0100

    WIP Alexis

--- src/wotr/Warrior.java
+++ src/wotr/Warrior.java
@@ -1,19 +1,25 @@
+WIP
 package wotr;
 
 public class Warrior extends NPC {
 	/**
-	 * Can kill a npc
+	 * A warrior is a specific npc. As like other npc he cans act on Frodo's healt points or corruption points. However he can also kill a npc.
+	 * When a npc is dead, he is removed from the frodo's fellowship and stay in the room where he died.
+	 * NPC can reanimate by a magician. A dead NPC can also be picked up again by frodo and his fellowship (because humans used to bury their dead friends...) 
 	 */
 	
 	private Player currentPlayer;
 
 
 	/**
-	 * @param name
-	 * @param description
-	 * @param corruptionPower
-	 * @param hpPower
-	 * @param currentPlayer
+	 *  * A warrior is a specific npc. As like other npc he cans act on Frodo's healt points or corruption points. However he can also kill a npc.
+	 * When a npc is dead, he is removed from the frodo's fellowship and stay in the room where he died.
+	 * NPC can reanimate by a magician. A dead NPC can also be picked up again by frodo and his fellowship (because humans used to bury their dead friends...) 
+	 *@param name : of the NPC
+     *@param description : a short description of th player ex : "dwarfs are sprinters, they are fast on short distances but long distances tires them."
+     *@param corruptionPower : /corruption power owned by the NPC, they are used to give corruption points to frodo
+     *@param hpPower : health points wich will be given or retrieved to Frodo
+     *@param currentPlayer : //frodo
 	 */
 	public Warrior(String name, String description, int corruptionPower, int hpPower, Player currentPlayer) {
 		super(name, description, corruptionPower, hpPower, currentPlayer);
@@ -28,7 +34,7 @@ public class Warrior extends NPC {
 	    */
 	    public boolean use(NPC npc){
 	        if (currentPlayer.hasNPC(npc)){
-	        npc.setAlreadyUsed(true);
+	        npc.setAlreadyUsed(true); //in order to don't pick him again and use his powers because he is dead. Possible if a magician restoreshis powers
 	        currentPlayer.dropNPC(npc);
 	        
 	        this.setAlreadyUsed(true);
