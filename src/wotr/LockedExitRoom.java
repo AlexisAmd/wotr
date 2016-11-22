13dc4ddbe864fc32ae39d50f94fbd885a7186b1e
salutlespotes <alexis.amiand@gmail.com>
2016-11-22 14:25:57 +0100

    Correcting Antoine's problems...

---
+++ src/wotr/LockedExitRoom.java
@@ -0,0 +1,37 @@
+// Work in progress - Antoine
+
+package wotr;
+
+/**
+ * Décrivez votre classe LockedExitRoom ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class LockedExitRoom extends ExitRoom
+{
+    protected boolean locked;
+    /**
+     * Constructor for locked doors
+     * @param this.currentRoom
+     * @param nextRoom
+
+     */
+    public LockedExitRoom(Room currentRoom,Room nextRoom)
+    {
+        super(currentRoom, nextRoom);
+        this.locked = true;
+    }
+
+    /**
+     * getLocked
+     * @return locked
+     */
+    public boolean getLocked(){
+        return locked;
+    }
+    
+    public void setLocked(Boolean bool){
+        locked=bool;
+    }
+}
