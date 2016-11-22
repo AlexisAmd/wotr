7513461af806a47ea5489678c13ae2980a0b79ce
adayre <ant.dayre@gmail.com>
2016-11-22 14:15:01 +0100

    bla

---
+++ src/wotr/wotr/ExitRoom.java
@@ -0,0 +1,44 @@
+//Work in progress - Antoine
+/**
+ * Class wich describes ExitRooms
+ * 
+ * @author GR1 - Alexis
+ * @version (a version number or a date)
+ */
+/**
+ * Class to manage exits (door)
+ * 
+
+ */
+package wotr;
+public class ExitRoom
+{
+    protected Room nextRoom;
+    protected Room currentRoom;
+    
+    /**
+     * Constructor for objects of class ExitRoom
+     * @param currentRoom the current room
+     * @param nextRoom the next room
+     */
+    public ExitRoom(Room currentRoom, Room nextRoom)
+    {
+        this.currentRoom=currentRoom;
+        this.nextRoom=nextRoom;
+    }
+
+    /**
+       *Methods to get the future current room after moving
+     * @param currentRoom
+     * @return the next room
+     */
+    public Room getNextRoom(Room nextRoom)
+    {
+        if (this.currentRoom!=nextRoom){
+            return this.currentRoom;
+        }
+        else{
+            return this.nextRoom;
+        }
+    }
+}
