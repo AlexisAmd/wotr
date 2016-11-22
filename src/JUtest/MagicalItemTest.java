01099ffd721934dcbf7a1c7ec09766f62a4c003f
agath <agath@172.17.79.210>
2016-11-22 15:16:25 +0100

    class MagicalItem and MagicalItemTest : Add comments and make tests

--- src/JUtest/MagicalItemTest.java
+++ src/JUtest/MagicalItemTest.java
@@ -1,43 +1,100 @@
 package JUtest;
 import static org.junit.Assert.*;
+
+import org.junit.Before;
+
 import wotr.*;
 import org.junit.Test;
 
 public class MagicalItemTest {
 
+	private Player myPlayer;
+	private MagicalItem magicalItem;
+	private Item item;
+	
+	/*
+	 * Sets up the test fixture 
+	 * Initialize one magicalItem for each test, whose name is "Ring", description is "Dangerous", weight is "12", corruptionPower is "40" and currentPlayer is myPlayer
+	 */
+	@Before
+	public void setUp()
+	{
+		myPlayer = new Player();
+		myPlayer.setCorruption(30); //Set the hp of the player
+		magicalItem = new MagicalItem("Ring", "Dangerous", 12, 40, myPlayer);
+	}
+	
+	/*
+	 * Method testName
+	 * Check if the name given as parameter in the constructor is correctly set to the attribute
+	 */
+	@Test
+	public void testName() {
+		assertEquals("Ring", item.getName());
+	}
+
+	/*
+	 * Method testDescription
+	 * Check if the description given as parameter in the constructor is correctly set to the attribute
+	 */
+	@Test
+	public void testDescription() {
+		assertEquals("Dangerous", item.getDescription());
+	}
+
+	/*
+	 * Method testWeight
+	 * Check if the weight given as parameter in the constructor is correctly set to the attribute
+	 */
+	@Test
+	public void testWeight() {
+		assertEquals(12, item.getWeight());
+	}
+	
+	/*
+	 * Method testCorruptionPower
+	 * Check if the corruptionPower given as parameter in the constructor is correctly set to the attribute
+	 */
+	@Test
+	public void testCorruptionPower() {
+		assertEquals(40, magicalItem.getCorruptionPower());
+	}
+
+	/*
+	 * Method testPlayer
+	 * Check if the player given as parameter in the constructor is correctly set to the attribute
+	 */
+	@Test
+	public void testPlayer() {
+		assertEquals(myPlayer, item.getCurrentPlayer());
+	}
+	
+	/*
+	 * Method testToString
+	 * Check if the information given as parameter in the constructor is correctly set to the attribute
+	 */
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		assertEquals("Name : Ring"+"\n"+"Description : Dangerous"+"\n"+"Weight : 12"+"\n" +"Corruption power : 40", magicalItem.toString());
 	}
 
+	/*
+	 * Method testUse
+	 * Check if the hp of the player are well upgraded
+	 */
 	@Test
 	public void testUse() {
-		fail("Not yet implemented");
+		magicalItem.use();
+		assertEquals(70, myPlayer.getPercentCorruption());
 	}
-
+	
+	/*
+	 * Method testGetCorruptionPower
+	 * Check if the corruptionPower given as parameter in the constructor is correctly set to the attribute
+	 * Check if the return value is the good one
+	 */
 	@Test
-	public void testMagicalItem() {
-		fail("Not yet implemented");
+	public void testGetCorruptionPower() {
+		assertEquals(40, magicalItem.getCorruptionPower());
 	}
-
-	@Test
-	public void testItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetDescription() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetWeight() {
-		fail("Not yet implemented");
-	}
-
 }
