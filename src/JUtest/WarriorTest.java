f7931cdd2a71348ca689cadc763ff1b6b66392aa
salutlespotes <alexis.amiand@gmail.com>
2016-11-22 16:51:16 +0100

    Class WarriorTest : creating the class

---
+++ src/JUtest/WarriorTest.java
@@ -0,0 +1,21 @@
+//WIP Alexis
+
+/**
+ * A magician can restore powers of an other NPC only one time. He can also give hps or corruption points (negative or positives ones) to frodo.
+ * For example Gandalf is a good magician, he decreases the corruption of frodo while saruman (the evil one) increase these corruption points but they can both restor the powers of an other npc.
+ *
+ * @author GR1
+ * @version 21/11/016
+ */
+package JUtest;
+import wotr.Warrior;
+import wotr.Player;
+
+/**
+ * @author GR1
+ * @version 22/11/2016
+ *
+ */
+public class WarriorTest {
+
+}
