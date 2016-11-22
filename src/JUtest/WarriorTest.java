07423121ca41562d280d787084a8aa0503faecf3
salutlespotes <alexis.amiand@gmail.com>
2016-11-22 17:23:01 +0100

    Class WarriorTest :  writting tests complete

--- src/JUtest/WarriorTest.java
+++ src/JUtest/WarriorTest.java
@@ -1,21 +1,72 @@
 //WIP Alexis
 
-/**
- * A magician can restore powers of an other NPC only one time. He can also give hps or corruption points (negative or positives ones) to frodo.
- * For example Gandalf is a good magician, he decreases the corruption of frodo while saruman (the evil one) increase these corruption points but they can both restor the powers of an other npc.
- *
- * @author GR1
- * @version 21/11/016
- */
+
 package JUtest;
 import wotr.Warrior;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import wotr.Magician;
+import wotr.NPC;
 import wotr.Player;
+import wotr.Room;
 
 /**
+ * 	
+ * A warrior is a specific npc. As like other npc he cans act on Frodo's healt points or corruption points. However he can also kill a npc.
+ * When a npc is dead, he is removed from the frodo's fellowship and stay in the room where he died.
+ * NPC can reanimate by a magician. A dead NPC can also be picked up again by frodo and his fellowship (because humans used to bury their dead friends...) 
+
  * @author GR1
  * @version 22/11/2016
  *
  */
 public class WarriorTest {
+	private Player Frodo;
+	private NPC npcSam;
+	private Warrior Aragorn;
+	private Room Forest;
+	
+	@Before
+	public void setUp() {
+		Frodo = new Player(); //hp = 100, corruption = 0
+		npcSam = new NPC("Sam", "Best Friend of Frodo", 5, 2, Frodo);
+		Aragorn = new Warrior("Aragorn", "Brave", -2, 0, Frodo);
+		Frodo.addNPC(npcSam);
+		Forest = new Room("forest");
+
+	}
+	
+	@Test //already tested in class player but checked again here
+	public void testAddSam(){
+		assertTrue(Frodo.getFellowship().contains(npcSam));//verify tham sam is a member of frodo's fellowship
+	}
+	
+	@Test
+	/**
+	 * If aragorn uses his powers on Sam we expect sam to be dead so to be out of the frodos inventory and without his powers
+	 * 
+	 * 	
+	 * */
+	public void testKillSam(){
+		Aragorn.use(npcSam);
+		assertTrue(npcSam.getAlreadyUsed()); //sam is dead, he does not have any powers now...
+		assertFalse(Frodo.getFellowship().contains(npcSam)); //sam is dead, he leaves to frodo's fellowship
+		
+	}
+	
+	@Test
+	/**
+	 * We want to know where the body of Sam is after killing him, it should be in the same place as frodo before frodo moves.
+	 */
+	public void testSamIsInTheForrest(){
+		Aragorn.use(npcSam);
+		assertTrue(Forest.getNPCList().contains(npcSam)); 
+	}
+
+	
 
 }
--- src/wotr/Warrior.java
+++ src/wotr/Warrior.java
@@ -36,7 +36,6 @@ public class Warrior extends NPC {
 	        if (currentPlayer.hasNPC(npc)){
 	        npc.setAlreadyUsed(true); //in order to don't pick him again and use his powers because he is dead. Possible if a magician restoreshis powers
 	        currentPlayer.dropNPC(npc); //drop the n
-	        
 	        this.setAlreadyUsed(true);
 	        return true;
 	    }
