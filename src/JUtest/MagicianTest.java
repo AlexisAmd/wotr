db9ae26635ae6f715c1447227c230f965ec291a3
salutlespotes <alexis.amiand@gmail.com>
2016-11-22 10:14:39 +0100

    Class MagicianTest : adding testUseNPC()
    To verify that a magician can restore powers of an other npc

--- src/JUtest/MagicianTest.java
+++ src/JUtest/MagicianTest.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import wotr.Magician;
 import wotr.NPC;
 import wotr.Player;
 
 public class MagicianTest {
 	private NPC npcSam;
-	private NPC npcGandalf;
+	private Magician magicianGandalf;
 	private Player Frodo;
 	
 
@@ -19,16 +20,16 @@ public class MagicianTest {
 	public void setUp() {
 		Frodo = new Player();
 		npcSam = new NPC("Sam", "Best Friend of Frodo", 5, 2, Frodo);
-		npcGandalf = new NPC("Gandalf", "A huge magician", 10, 10, Frodo);
+		magicianGandalf = new Magician("Gandalf", "A huge magician", 10, 10, Frodo);
 	}
 
 
 	
 	@Test
 	public void testUseNPC(){
-		npcSam.use();
-		assertEquals(102, Frodo.getHp());
-		assertEquals(5, Frodo.getPercentCorruption());
+		npcSam.use(); //alreadyuse for sam goes to true
+		magicianGandalf.use(npcSam); //should restore powers of sam
+		assertEquals(false, npcSam.getAlreadyUsed()); //verifying that the powers of sam are restored
 		
 	}
 		
