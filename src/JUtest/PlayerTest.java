f0d93baecb3eea54252bfb78d3f614ec333535d5
Pierre <Pierre@172.18.28.241>
2016-11-22 10:44:18 +0100

    #Player and PlayerTest changes
    Added isAlive and descriptions to Player.
    Added tests and descriptions to PlayerTest.

--- src/JUtest/PlayerTest.java
+++ src/JUtest/PlayerTest.java
@@ -1,99 +1,157 @@
 package JUtest;
 import static org.junit.Assert.*;
-import wotr.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+/**
+ * The test class PlyerTest.
+ * Tests the methods of the clas "Player".
+ * 
+ * @author GR1
+ * @version 22/11/2016
+ */
 
 public class PlayerTest {
 
-	private Player player;
+	private Player myPlayer;
 	
-	protected void setUp() {
+	/**
+	 * Default constructor for the PlayerTest class.
+	 */
+	public PlayerTest()
+	{
+		
+	}
+	
+	/**
+	 * Sets up the test.
+	 * Called before every test case method.
+	 * Creates a Player instance.
+	 */
+	@Before
+	public void setUp()
+	{
 		player = new Player();
 	}
 	
-	@Test
-	public void testGame() {
-		assertNotNull("Instance is not created!", player);
+	/**
+	 * Tears down the current test.
+	 * This method is called after every test case method.
+	 */
+	@Before
+	public void tearDown()
+	{
+		
 	}
-
+	
+	/**
+	 * Method to simulate the player's death.
+	 * Player receives a big amount of damage.
+	 * testIsALive then check if he is considered as dead.
+	 */
+	public void testIsAlive()
+	{
+		myPlayer.addHP(-1000);
+		// Assuming that 
+	}
 	
 	@Test
-	public void testGetName() {
-		assertEquals("Player's name is not Frodo!", "Frodo", player.getName());
-	}
-
-	@Test
-	public void testGetHp() {
+	public void testGetHp()
+	{
 		assertEquals("Player's HP does not match!", 100, player.getHp());
 	}
+	
+	@Test
+	public void testSetHp()
+	{
+		player.setHp(50)
+		assertEquals("Player's HP was not set correctly!", 50, player.getHp());
+	}
+	
+	@Test
+	public void testGetPercentCorruption()
+	{
+		assertEquals("Player's corruption does not match!", 0, player.getCorruption());
+	}
+	
+	@Test
+	public void testAddHp()
+	{
+		player.setHp(50);
+		player.addHp(10);
+		assertEquals("Healing does not work as intended", 60, player.getHp());
+	}
 
 	@Test
-	public void testGetPercentCorruption() {
+	public void testAddCorruption()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testAddHp() {
+	public void testGetFellowship()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testAddCorruption() {
+	public void testHadNPC()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testGetFellowship() {
+	public void testAddNPC()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testHadNPC() {
+	public void testGetInventory()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testAddNPC() {
+	public void testGetInventoryWeight()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testGetInventory() {
+	public void testGetMaximumInventoryWeight()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testGetInventoryWeight() {
+	public void testHasItem()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testGetMaximumInventoryWeight() {
+	public void testAddItem()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testHasItem() {
+	public void testUseItem()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testAddItem() {
+	public void testUseNPC()
+	{
 		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testUseItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUseNPC() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
+	public void testToString()
+	{
 		fail("Not yet implemented");
 	}
 
--- src/wotr/Player.java
+++ src/wotr/Player.java
@@ -2,14 +2,16 @@ package wotr;
 import java.util.*;
 
 /**
- * Player class stands for a World of the Ring player able to epxplore many differents world. This player is named FRODO.
+ * Player class stands for a World of the Ring player able to explore many differents world. This player is named Frodo.
  * A player have health point and corruption points. When healt point is equal to 0 the player is dead. When the percentage of corruption of the player is equal to 100% the payer is dead.
- *A player have an inventory wich contains items and a fellowship which contains NPC.
- *This inventory have a maximum weight.
- *By default, the player have 100 hp and 0 % of corruption
- *@author GR1
- *@version 21/11/16
+ * A player have an inventory wich contains itms and a fellowship which contains NPC.
+ * This inventory have a maximum weight.
+ * By default, the player have 100 hp and 0 % of corruption
+ * @author GR1
+ * @version 21/11/16
  */
+
+
 public class Player
 {
 
@@ -17,7 +19,7 @@ public class Player
     private int hp;//health points of the player, can be >100 but if <=0, the player is dead
     private int percentCorruption; //percentage of corruption, cannot be <0, player's die if percentage is >=100
     private ArrayList<Item> inventory; //list of all items stored in the player's inventory
-    private ArrayList<NPC> fellowship; //list of all friends of our hero
+    private ArrayList<NPC> fellowship; //list of all friends currently following the hero
     private int inventoryWeight; //current weigh of the inventory
     private int maximumInventoryWeight; //maximum weight of th inventory
 
@@ -27,49 +29,76 @@ public class Player
      *
      */
     public Player()
-    { //constructeur sans parametres ?
+    {   
+    	//constructeur sans parametres ?
+        // initialise instance variables
         name = "Frodo";
         hp= 100;
         percentCorruption = 0;
         inventory = new ArrayList<>();
         fellowship = new ArrayList<>();
-
     }
-
+    
+    /**
+     * Checks if the player is still alive.
+     * Player is alive only if his HP are strictly superior to 0.
+     * @return true if the player's HP are strictly higher than 0.
+     */
+    private boolean isAlive()
+    {
+    	return hp > 0;
+    }
 
     /**
-     * getHp : return the number of healthpoints
+     * Returns the number of health points.
      * @return
      */
-    public int getHp(){
+    public int getHp()
+    {
         return hp;
     }
+    
+    /**
+     * setHp : sets a new number of healthpoints
+     * @param newHpValue The new value of the player's health points.
+     */
+    public void setHp(int newHpValue)
+    {
+    	if ((newHpValue < 0) || (newHpValue > 100))
+    		throw new IllegalArgumentException("Value is out of range for newHpValue");
+    	this.hp = newHpValue;
+    }
+	
 
     /**
      * getHp : return the perentage of corruption of the player
      * @return
      */
-    public int getPercentCorruption(){
+    public int getPercentCorruption()
+    {
         return percentCorruption;
     }
 
     /**
-     * Manage the gain and the loose of player's healt point
-     * Hp can be >100 but if HP<= 0 the player is dead, the game is over
-     * @param nb the number of health points to gain, can be a negative
-     */
-    public void addHp(int nb){
-        //verify if <0 
-        hp+=nb;
-    }
+	 * Manage the gain of one life
+	 * @param nb The number of health points to gain, can be a negative value.
+	 */
+	public void addHp(int nb)
+	{
+		if ((100 - hp) <= nb)
+			hp=100;
+		else
+			hp+=nb;
+	}
 
     /**
      * Manage the loose and the of corruption
      * If corruption reach 100%, then the game is over
      * cannot be < 0
-     * @param nb the number of corruption to loose, can be a negative value
+     * @param nb The number of corruption to loose, can be a negative value
      */
-    public void addCorruption(int nb){
+    public void addCorruption(int nb)
+	{
         //verify if <0 or >100
         percentCorruption+=nb;
     }
@@ -79,30 +108,36 @@ public class Player
      * Method to return the feelowship of the hero
      * @return the fellowship of the hero
      */
-    public ArrayList<NPC> getFellowship(){
+    public ArrayList<NPC> getFellowship()
+    {
         return fellowship;
     }
 
     /**
      * Check if the player have this friend
-     * @param npc, the npc you want to find
+     * @param npc The npc you want to find.
      * @return true if the player have this npc
      */
-    public boolean hadNPC(NPC npc){
+    public boolean hadNPC(NPC npc)
+    {
         return fellowship.contains(npc);
     }
 
     /**
      * Add a friend to the player's fellowship
-     * @param npc the non player character you just met
-     *@return true if added
+     * @param npc The non player character you just met.
+     * @return true if added
      */
-    public boolean addNPC(NPC npc){
-        if (!hadNPC(npc)){    
+    public boolean addNPC(NPC npc)
+    {
+        if (!hadNPC(npc))
+        {    
             fellowship.add(npc); //add the this new friend to the fellowship
             //System.out.println(npc.getName()+" is now a member of the fellowship of the ring.");
-            return true;}
-        else{
+            return true;
+        }
+        else
+        {
             //System.out.println(npc.getName()+" is already a member of the fellowship of the ring.");
             return false;
         }
@@ -110,50 +145,60 @@ public class Player
 
 
     /**
-     * getinventory : return the items's list
+     * Returns the list of items
      * @return the inventory of our hero
      */
-    public ArrayList<Item> getInventory(){
+    public ArrayList<Item> getInventory()
+    {
         return inventory;
     }
 
     /**
-     * getWeight : return the sum of the weight of the items
+     * Returns the total weight of the items the player carries.
      * @return
      */
-    public int getInventoryWeight(){
+    public int getInventoryWeight()
+    {
         return inventoryWeight;
     }
 
     /**
-     * getMaximumWeight : return the maximum weight
-     * @return
+     * Returns the maximum weight that the player can carry.
+     * @return the value of the maximum weight
      */
-    public int getMaximumInventoryWeight(){
+    public int getMaximumInventoryWeight()
+    {
         return maximumInventoryWeight;
     }
 
     /**
-     * Check if the player have this item
+     * Check if the player have a specific item.
      * @param item the item you want to check
-     * @return true if the player have this item
+     * @return true if the player has this item
      */
-    public boolean hasItem(Item item){
+    public boolean hasItem(Item item)
+    {
         return inventory.contains(item);
     }
 
     /**
-     * Add the item to the player's list if th  object is not already present and its weight is not superior to the maximum weight
-     * The inventory is allowed to contains the same object many times (i.e it can contains 6 beers)
-     *@param newItem
+     * Add the item to the player's inventory.
+     * Only if its added weight does not make the total inventory weight
+     * superior to the maximum weight.
+     * A player can carry many items of the same type.
+     * (i.e it can contains 6 beers)
+     * @param newItem the new item in the player's inventory
      * @return true if the item has been added to the inventoru
      */
-    public boolean addItem(Item newItem){
-        if(newItem.getWeight()+inventoryWeight>maximumInventoryWeight){
+    public boolean addItem(Item newItem)
+    {
+        if(newItem.getWeight()+inventoryWeight>maximumInventoryWeight)
+        {
             //System.out.println("Objet too heavy for your inventory (item weight : "+newItem.getWeight()+" ) !");
             return false;
         }
-        else{
+        else
+        {
             inventory.add(newItem); //add the item to the inventory
             inventoryWeight+=newItem.getWeight(); //update the actual weight of th inventory
             return true;
@@ -161,13 +206,16 @@ public class Player
     }
 
     /**
-     * Remove a specified item from the player's inventory. If the item is deleted the total weight of the inventory decrease according to the weight of the item.
-     * @param item item to remove
+     * Remove a specified item from the player's inventory.
+     * If the item is deleted the total weight of the inventory decrease
+     * according to the weight of the item.
+     * @param item the item to remove
      * @return false if not done
      */
-    private boolean dropItem(Item item)
+    private boolean dropItem (Item item)
     {
-        if (inventory.contains(item)){
+        if (inventory.contains(item))
+        {
             inventoryWeight-=item.getWeight();
         }
         return inventory.remove(item);//retrun false if not deleted
@@ -178,21 +226,29 @@ public class Player
 
     
     /**
-    * Allow the player to use an item. Item can have an action on the world and/or on the player's hp and/or cp.
-    */
-    public void use(Item item){
+     * Allow the player to use an item.
+     * Items can have an action on the world and/or on the player's HP and/or CP.
+     * @param item the item that the player wants to use
+     */
+    public void use(Item item)
+    {
 
-        item.use();//usefull for keys
+        item.use(); //usefull for keys
         
-        if (!item.getClass().getName().equals("Key")){//keys cannot be used more than one time
+        if (!item.getClass().getName().equals("Key"))
+        {
+        	//keys cannot be used more than one time
             dropItem(item);
         }
     }
     
     /**
-    * Allow the player to use a NPC. NPC can have an action on the world and/or on the player's hp and/or cp.
-    **/
-    public void use(NPC npc){
+	 * Allow the player to use a NPC.
+	 * NPC can have act on the world and/or on the player's HP and/or CP.
+	 * @param npc the NPC that the player wants to use.
+	 */
+    public void use(NPC npc)
+    {
         npc.use();
     }
 
@@ -201,16 +257,19 @@ public class Player
      * Return the player's informations as a string
      * @return description of the player
      */
-    public String toString(){
+    public String toString()
+    {
         String str;    
         str=" - "+name+" - "+"\n"+
         "Inventory capacity : "+inventoryWeight+"/"+maximumInventoryWeight+"\n"+
         "Item List : "+"\n";
-        for(Item item : inventory){
+        for(Item item : inventory)
+        {
             str+=" - "+item.toString()+"\n";
         }
         str+="Fellowship : "+"\n";
-        for(NPC npc : fellowship){
+        for(NPC npc : fellowship)
+        {
             str+=" - "+npc.toString()+"\n";
         }
         return str;
