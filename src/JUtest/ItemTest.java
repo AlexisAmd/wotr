3a5b82e6b45e1bdcd18fd7b75a3243ab64830293
Caroline <Caroline@PC_Caroline>
2016-11-22 15:12:59 +0100

    test class item - to delete

--- src/JUtest/ItemTest.java
+++ src/JUtest/ItemTest.java
@@ -4,15 +4,51 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 
 import wotr.*;
+import wotr.Item;
 import org.junit.Test;
+import org.junit.After;
+
+/**
+ * Test class of the class Item.
+ * Tests the methods of the class Item.
+ * @author GR1
+ * @version 22/11/2016
+ */
 
 
-public class ItemTest {
+public class ItemTest<myItem> {
 
 	private Item myItem;
+	private Player currentPlayer;
+	
+	/**
+	 * Default constructor for the ItemTest class.
+	 */
+	public ItemTest()
+	{
+		
+	}
+	
+	/**
+	 * Sets up the test.
+	 * Called before every test case method.
+	 * Creates an Item instance.
+	 */
+	@Before
+	public void setUp()
+	{
+		currentPlayer = new Player();
+		itemBier = new Item("Bier","I give health points.",10, currentPlayer);
+	}
 
-	protected void setUp() {
-		myItem = new Item ("Bier","I give health points.",10,player1);
+	/**
+	 * Tears down the current test.
+	 * This method is called after every test case method.
+	 */
+	@After
+	public void tearDown()
+	{
+		
 	}
 
 	@Test
@@ -44,5 +80,9 @@ public class ItemTest {
 	public void testUse() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void testGetCurrentPlayer() {
+		fail("Not yet implemented");
+	}	
 }
