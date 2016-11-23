17c5688dfdaef27972039b52bac62416f7be84f0
Alexis A <alexis.amiand@gmail.com>
2016-11-23 20:25:05 +0100

    Adding Test

---
+++ src/JUtest/FellowshipTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package JUtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author pc
+ *
+ */
+public class FellowshipTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link wotr.Fellowship#Fellowship()}.
+	 */
+	@Test
+	public void testFellowship() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wotr.Fellowship#getFellowship()}.
+	 */
+	@Test
+	public void testGetFellowship() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wotr.Fellowship#hasNPC(wotr.NPC)}.
+	 */
+	@Test
+	public void testHasNPC() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wotr.Fellowship#addNPC(wotr.NPC)}.
+	 */
+	@Test
+	public void testAddNPC() {
+		fail("Not yet implemented");
+	}
+
+}
