5404f8e6910921e0dd4d0aeb697df3501449afb1
adayre <ant.dayre@gmail.com>
2016-11-23 12:06:41 +0100

    new class

---
+++ src/wotr/WorldExit.java
@@ -0,0 +1,23 @@
+package wotr;
+
+public class WorldExit extends ExitRoom {
+
+	private Player player;
+	
+	
+	
+	
+	
+	
+	public WorldExit(Room currentRoom, Room nextRoom) {
+		super(currentRoom, nextRoom);
+
+	}
+
+	
+	
+	
+	
+	
+	
+}
