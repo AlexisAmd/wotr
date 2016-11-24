395a01260065a18f3a7490819ae01f868ff4268f
adayre <ant.dayre@gmail.com>
2016-11-24 08:36:42 +0100

    BLA

---
+++ src/JUtest/FellowshipTest.java
@@ -0,0 +1,32 @@
+package JUtest;
+import wotr.*;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.*;
+
+public class FellowshipTest {
+
+	private Fellowship fellowship;
+	private Player player;
+	private NPC npc;
+	
+	
+	@Before
+	public void setUp(){
+		player = new Player();
+		npc = new NPC("Sam", "hobbit", 0, 100, player);
+		fellowship = new Fellowship();
+		fellowship.addNPC(npc);
+	}
+
+	@Test
+	public void testGetFellowship(){
+		assertNotNull("not null",fellowship.getFellowship());
+		
+	}
+	
+}
