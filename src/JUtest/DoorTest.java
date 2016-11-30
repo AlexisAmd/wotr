7f726174ebbfbe72dff70b302098e7e59ede3818
Pierre <Pierre@172.18.28.241>
2016-11-30 10:58:49 +0100

    Changed ExitRoom into Door in ExitRoomTest

--- src/JUtest/DoorTest.java
+++ src/JUtest/DoorTest.java
@@ -1,21 +1,18 @@
-/*Work on progress - Antoine */
 
 package JUtest;
 import junit.framework.*;
-import wotr.ExitRoom;
+import wotr.Door;
 import wotr.Room;
 
-public class ExitRoomTest extends TestCase {
-	private ExitRoom room;
+public class DoorTest extends TestCase {
+	private Door room;
 	private Room currentRoom;
 	private Room nextRoom;
 
 	public void testGetNextRoom() {
 		currentRoom = new Room("salle1");
 		nextRoom = new Room ("salle2");
-		room = new ExitRoom(currentRoom,nextRoom);
+		room = new Door(currentRoom,nextRoom);
 		assertNotNull("not null",room.getNextRoom(currentRoom));
 	}
 }
-
-
