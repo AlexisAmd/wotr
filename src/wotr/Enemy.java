5df5a6a66b212a0751f5e984d658234d1f9bc5d3
Alexis A <alexis.amiand@gmail.com>
2016-12-05 16:30:58 +0100

    Ok tryToAttack ! :+1:  solving issues of rights..

--- src/wotr/Enemy.java
+++ src/wotr/Enemy.java
@@ -2,24 +2,19 @@ package wotr;
 
 public class Enemy extends NPC{
 
-	private Player currentPlayer;
-	private int hpPower;
-	private int corruptionPower;
+
 
 	
 	
 	public Enemy(String name, String description, int corruptionPower, int hpPower, Player currentPlayer) {
 		super(name, description, corruptionPower, hpPower, currentPlayer);
-		this.currentPlayer = currentPlayer;
-		this.corruptionPower= corruptionPower;
-		this.hpPower = hpPower;
 	}
 	
 	public boolean use(){
 		
 		if (!this.getAlreadyUsed()){
-		currentPlayer.addHp(hpPower); // we will put a number below 0 to make the player loose health points.
-		currentPlayer.addCorruption(corruptionPower);
+		super.getPlayer().addHp(super.getHpPower()); // we will put a number below 0 to make the player loose health points.
+		super.getPlayer().addCorruption(super.getCpPower());
 		//this.setAlreadyUsed(true);
 		return true;}
 		else return false;
